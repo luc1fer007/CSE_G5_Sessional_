ee802e90fac9a86e076d8463b312829e360e89ab
lucifer <arifulhassanjoy02@gmail.com>
2026-01-10 22:52:36 +0600

    Add Maximum Element Using BST implementation
    Implement a binary search tree and an in-order traversal to find the maximum element.

---
+++ Tree_Traversal/Maximum_Element_Using_BST.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node *left;
+    node *right;
+public:
+    node(int x)
+    {
+        data = x;
+        left = right = nullptr;
+    }
+};
+int inOrder(node *root, vector<int>&sorted_Vec)
+{
+    if(root == nullptr)return 0;
+    inOrder(root->left, sorted_Vec);
+    sorted_Vec.push_back(root->data);
+    inOrder(root->right, sorted_Vec);
+    return sorted_Vec.back();
+}
+int main()
+{
+    node *root = new node(9);
+    root->left = new node(5);
+    root->right = new node(10);
+    root->left->left = new node(2);
+    root->left->right = new node(7);
+    root->right->left = new node(12);
+    root->right->right = new node(13);
+
+    vector<int>sorted_Vec;
+    int res = inOrder(root, sorted_Vec);
+    cout<<res<<endl;
+    return 0;
+}
