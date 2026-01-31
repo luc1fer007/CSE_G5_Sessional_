e8ad706da78a89818e9b9848c359cbe037329108
lucifer <arifulhassanjoy02@gmail.com>
2026-01-31 13:58:17 +0600

    Add extraction and insertion operator overloading
    Implemented extraction and insertion operators using friend functions for the Demo class.

---
+++ Operator_Overloading/Extraction_Operator_Using_Friend_Function.cpp
@@ -0,0 +1,30 @@
+/*Create a class Demo and declare two variables inside the class. Overload the extraction operator
+">>" to read the inputs from the user and overload the insertion operator "<<" to print the values.
+Code Implementation:*/
+//if class members are private use friend function
+#include<bits/stdc++.h>
+using namespace std;
+
+class demo
+{
+    int length, breadth;
+    friend istream& operator>>(istream&in, demo &ob);
+    friend ostream& operator<<(ostream&out, demo &ob);
+};
+istream& operator>>(istream&in, demo &ob)
+{
+    in>>ob.length>>ob.breadth;
+    return in;
+}
+ostream& operator<<(ostream&out, demo &ob)
+{
+    out<<ob.length<<" "<<ob.breadth;
+    return out;
+}
+int main()
+{
+    demo obj;
+    cin>>obj;
+    cout<<obj;
+    return 0;
+}
